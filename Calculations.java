17299176e279bf6e2451d2df10553048376a7398
Ahmed <victorylover987@gmail.com>
2020-06-11 13:31:31 +0400

    Division is implemented

--- Calculations.java
+++ Calculations.java
@@ -8,10 +8,16 @@ public class Calculations {
 		int sum = x+y;
 		System.out.println("The sum is= "+ sum);
 		
+		int sub = y -x;
+		System.out.println("The sub is= "+ sub);
 		//member_B add the feature of subtracting x from y and print the result
 		
+		int Div = x/y;
+		System.out.println("The Div is= "+ Div);
 		//member_C add the feature of dividing x by y and print the result
 		
+		int Mul = x*y;
+		System.out.println("The Mul is= "+ Mul);
 		//member_D add the feature of multiplying x by y and print the result
 		
 		
